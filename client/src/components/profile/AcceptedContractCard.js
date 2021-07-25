f72698cbc61e60191d0aa41697fdd60771be4e9a
Rafsani <1605119@ugrad.cse.buet.ac.bd>
2021-07-26 04:33:36 +0600

    added Acctepted contract in lender profile :ok_hand:

---
+++ client/src/components/profile/AcceptedContractCard.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+
+import "./profile.css";
+
+import formatDate from "../../utils/formatDate";
+
+function AcceptedCOntractsCard({
+  contractDetails,
+  userId,
+  targetId,
+  viewAsLender,
+  viewButtons,
+}) {
+  return (
+    <div>
+      <div class="card-profile">
+        <Link
+          to={{
+            pathname: "/lenderViewsReceiver",
+            lenderViewsReceiver: viewAsLender,
+            lenderId: userId,
+            receiverId: targetId,
+          }}
+        >
+          <div class="photo-space"></div>
+        </Link>
+        <div class="user-info">
+          <div class="name">{contractDetails.receiverName}</div>
+          <div class="loan-contract-details">
+            <div class="item">
+              <div class="total">
+                <span class="field">Amount:</span>{" "}
+                <span class="value">
+                  {Math.round(contractDetails.amount)} BDT
+                </span>
+              </div>
+              <div class="collected">
+                <span class="field">Collected Amount:</span>{" "}
+                <span class="value">
+                  {Math.round(contractDetails.collectedAmount)} BDT
+                </span>
+              </div>
+            </div>
+            <div class="item">
+              <div class="next-installment">
+                <span class="field">Next Installment:</span>{" "}
+                <span class="value">
+                  {formatDate(contractDetails.nextInstallmentDate)}
+                </span>
+              </div>
+              <div class="amount">
+                <span class="field">Installment: </span>{" "}
+                <span class="value">
+                  {Math.round(contractDetails.nextInstallmentAmount)} BDT
+                </span>
+              </div>
+            </div>
+            <div class="item">
+              <div class="installment-values">
+                <div class="centering">
+                  <span class="field">Installments Completed: </span>{" "}
+                  <span class="value">
+                    {" "}
+                    {contractDetails.installmentsCompleted}{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="interest-rate">
+                <span class="field">Interest Rate:</span>{" "}
+                <span class="value"> {contractDetails.interestRate} % </span>
+              </div>
+              <div class="default">
+                <span class="field">Contract Defaults: </span>{" "}
+                <span class="value">
+                  {contractDetails.contractDefaults} times
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {viewButtons && (
+          <div class="small-buttons-list">
+            <div class="buttons ">
+              <a href="#" class="small-btn-profile btn-dark">  
+                proceed To Payment
+              </a>
+            </div>
+           
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AcceptedCOntractsCard;
--- client/src/components/profile/lenderProfile.js
+++ client/src/components/profile/lenderProfile.js
@@ -9,6 +9,7 @@ import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 import formatDate from "../../utils/formatDate";
 import BasicInfo from "./basicInfo";
 import CurrentlyLendingCard from "./currentlyLendingCard";
+import AcceptedCOntractsCard from "./AcceptedContractCard";
 
 function LenderProfile({ userId, hiddenData }) {
   const [lenderHistory, setLenderHistory] = useState(null);
@@ -108,12 +109,49 @@ function LenderProfile({ userId, hiddenData }) {
     );
   };
 
+
+const showAcceptedOffers = () => {
+  return (
+    lenderHistory &&
+    lenderHistory.currentlyActiveContacts &&
+    lenderHistory.currentlyActiveContacts.length > 0 && (
+      <div class="side-scroll-section content-box " id="side-scroll-section">
+        <div class="currently-lending " id="currently-lending">
+          <h1>
+            Accepted Contracts
+            <i
+              class="fas fa-flag-checkered "
+              data-tooltip="Reached Your Limit For Lenders"
+            ></i>
+          </h1>
+          <div class="user-cards scroller">
+            {lenderHistory.currentlyActiveContacts &&
+              lenderHistory.currentlyActiveContacts.map((contract) => (
+                <AcceptedCOntractsCard
+                  key={contract.contractId}
+                  contractDetails={contract}
+                  userId={userId}
+                  targetId={contract.receiverId}
+                  viewAsLender={true}
+                  viewButtons={true}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+
+
   return (
     <div>
       <main class="" id="main">
         <BasicInfo hiddenData={hiddenData} />
         {showLenderHistory()}
         {showCurrentlyLending()}
+        {showAcceptedOffers()}
       </main>
     </div>
   );
